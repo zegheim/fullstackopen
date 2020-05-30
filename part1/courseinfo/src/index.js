3106f0693398ffd8c94dd52796efebbadef29c22
zegheim <justinarista@gmail.com>
2020-05-31 00:10:36 +0700

    Finished exercise 1.5

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const Header = (props) => {
-  return <h1>{props.course}</h1>;
+  return <h1>{props.course.name}</h1>;
 };
 
 const Part = (props) => (
@@ -13,34 +13,45 @@ const Part = (props) => (
 
 const Content = (props) => (
   <>
-    <Part title={props.parts[0].name} amount={props.parts[0].exercises} />
-    <Part title={props.parts[1].name} amount={props.parts[1].exercises} />
-    <Part title={props.parts[2].name} amount={props.parts[2].exercises} />
+    <Part
+      title={props.course.parts[0].name}
+      amount={props.course.parts[0].exercises}
+    />
+    <Part
+      title={props.course.parts[1].name}
+      amount={props.course.parts[1].exercises}
+    />
+    <Part
+      title={props.course.parts[2].name}
+      amount={props.course.parts[2].exercises}
+    />
   </>
 );
 
 const Total = (props) => (
   <p>
     Number of exercises{" "}
-    {props.parts[0].exercises +
-      props.parts[1].exercises +
-      props.parts[2].exercises}
+    {props.course.parts[0].exercises +
+      props.course.parts[1].exercises +
+      props.course.parts[2].exercises}
   </p>
 );
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
 
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </>
   );
 };
