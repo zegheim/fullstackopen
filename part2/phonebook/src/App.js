23ec59099053f15612bfc654e6a88abddc839ca0
zegheim <justinarista@gmail.com>
2020-06-01 20:55:03 +0700

    finished exercise 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,11 +4,19 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
   const [newName, setNewName] = useState("");
 
+  const isDifferentFrom = (target) => (ref) => ref.name !== target;
+
   const addPerson = (event) => {
     event.preventDefault();
-    const person = { name: newName };
-    setPersons(persons.concat(person));
-    setNewName("");
+
+    const isUnique = persons.every(isDifferentFrom(newName));
+    if (isUnique) {
+      const person = { name: newName };
+      setPersons(persons.concat(person));
+      setNewName("");
+    } else {
+      alert(`${newName} is already added to phonebook`);
+    }
   };
 
   const handleFormChange = (event) => {
