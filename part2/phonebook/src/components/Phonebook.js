89f04f4ef43c2459fb192fa2a6f6d4db9beea572
zegheim <justinarista@gmail.com>
2020-06-01 21:36:38 +0700

    finished exercise 2.10

--- part2/phonebook/src/components/Phonebook.js
+++ part2/phonebook/src/components/Phonebook.js
@@ -1,10 +1,11 @@
 import React from "react";
 
+const Entry = (props) => (
+  <p>
+    {props.person.name} {props.person.number}
+  </p>
+);
 const Phonebook = (props) =>
-  props.persons.map((person) => (
-    <p key={person.number}>
-      {person.name} {person.number}
-    </p>
-  ));
+  props.persons.map((person) => <Entry key={person.number} person={person} />);
 
 export default Phonebook;
