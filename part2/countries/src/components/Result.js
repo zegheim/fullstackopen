d78e62caee58a5055ec860369260793931e12e17
zegheim <justinarista@gmail.com>
2020-06-02 18:55:31 +0700

    finished exercise 2.14

--- part2/countries/src/components/Result.js
+++ part2/countries/src/components/Result.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Result = ({ countries }) => {
   if (countries.length > 10) {
@@ -46,9 +47,56 @@ const DetailedResult = ({ country }) => {
       <p>population {country.population}</p>
       <h3>languages</h3>
       <ul>{languages}</ul>
-      <img src={country.flag} width="100" />
+      <img src={country.flag} alt={`Flag of ${country.name}`} width="100" />
+      <Weather city={country.capital} />
     </div>
   );
+};
+
+const Weather = ({ city, countryCode }) => {
+  const [weather, setWeather] = useState({
+    isReady: false,
+  });
+
+  const params = {
+    appid: process.env.REACT_APP_API_KEY,
+    q: `${city},${countryCode}`,
+    units: "metric",
+  };
+
+  const getWeather = () => {
+    axios
+      .get("https://api.openweathermap.org/data/2.5/weather", { params })
+      .then((response) => {
+        setWeather({ isReady: true, ...response.data });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  useEffect(getWeather, []);
+
+  if (weather.isReady) {
+    return (
+      <div>
+        <h3>Weather in {city}</h3>
+        <p>
+          <strong>temperature:</strong> {weather.main.temp} Celsius
+        </p>
+        <img
+          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
+          alt={`${weather.weather[0].description} icon`}
+          width="50"
+        />
+        <p>
+          <strong>wind:</strong> {weather.wind.speed} m/s, {weather.wind.deg}°
+        </p>
+      </div>
+    );
+  }
+
+  return <p>Loading weather data...</p>;
 };
 
 export default Result;
