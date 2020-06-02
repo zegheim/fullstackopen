3719fa7ea2b7a1fe7c4719fedd6d1d017d572a43
zegheim <justinarista@gmail.com>
2020-06-02 15:41:47 +0700

    finished exercise 2.13

--- part2/countries/src/components/Result.js
+++ part2/countries/src/components/Result.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Result = ({ countries }) => {
   if (countries.length > 10) {
     return <p>Too many matches, specify another filter</p>;
   } else if (countries.length > 1) {
-    return countries.map((c) => <p key={c.alpha2Code}>{c.name}</p>);
+    return countries.map((c) => (
+      <ToggledResult key={c.alpha2Code} country={c} />
+    ));
   } else if (countries.length === 1) {
     return <DetailedResult country={countries[0]} />;
   } else {
     return <p>No matches found, try relaxing your filter</p>;
   }
+};
+
+const ToggledResult = ({ country }) => {
+  const [isVisible, setVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    setVisible(!isVisible);
+  };
+
+  return (
+    <>
+      <div>
+        {country.name}{" "}
+        <button onClick={toggleVisibility}>
+          {isVisible ? "hide" : "show"}
+        </button>
+      </div>
+      {isVisible ? <DetailedResult country={country} /> : null}
+    </>
+  );
 };
 
 const DetailedResult = ({ country }) => {
