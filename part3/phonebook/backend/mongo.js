27d0a55c2c523f808816d03f1fd832e816e3e132
zegheim <justinarista@gmail.com>
2020-06-03 23:35:15 +0700

    updated mongo.js to use models/person module

--- part3/phonebook/backend/mongo.js
+++ part3/phonebook/backend/mongo.js
@@ -1,28 +1,15 @@
-const mongoose = require("mongoose");
+const Person = require("./models/person");
 
 const args = process.argv.slice(2);
 
-if (![1, 3].includes(args.length)) {
+if (![0, 2].includes(args.length)) {
   console.log("Please provide either:");
-  console.log("  1. Password ONLY to display all entries in the phonebook, or");
-  console.log(
-    "  2. Password, name, and number to save a new entry to the phonebook."
-  );
+  console.log("  1. no arguments display all entries in the phonebook, or");
+  console.log("  2. name, and number to save a new entry to the phonebook.");
   process.exit(1);
 }
 
-const [password, newName, newNumber] = args;
-
-const url = `mongodb+srv://fullstack:${password}@fullstackopen-a7hji.mongodb.net/note-app?retryWrites=true&w=majority`;
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-});
-
-const Person = mongoose.model("Person", personSchema);
+const [newName, newNumber] = args;
 
 if (newName && newNumber) {
   const person = new Person({
