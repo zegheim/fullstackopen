b360266138de643e8bde629d1b4f6c53df79fd8f
zegheim <justinarista@gmail.com>
2020-06-03 18:13:41 +0700

    uibuild

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 
 morgan.token("data", (req, res) => JSON.stringify(req.body));
