addd8d54e3439294269e1fcdb017fa6bcce20e54
zegheim <justinarista@gmail.com>
2020-06-03 15:31:40 +0700

    finished exercise 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,11 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("data", (req, res) => JSON.stringify(req.body));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
 
 let persons = [
   {
