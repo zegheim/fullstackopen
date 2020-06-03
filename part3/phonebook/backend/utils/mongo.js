ca99b47975a42b46b45d03263e066d22f47cb18e
zegheim <justinarista@gmail.com>
2020-06-03 23:44:02 +0700

    Revert "updated mongo.js to use models/person module"
    This reverts commit 27d0a55c2c523f808816d03f1fd832e816e3e132.

--- part3/phonebook/backend/utils/mongo.js
+++ part3/phonebook/backend/utils/mongo.js
@@ -1,15 +1,28 @@
-const Person = require("./models/person");
+const mongoose = require("mongoose");
 
 const args = process.argv.slice(2);
 
-if (![0, 2].includes(args.length)) {
+if (![1, 3].includes(args.length)) {
   console.log("Please provide either:");
-  console.log("  1. no arguments display all entries in the phonebook, or");
-  console.log("  2. name, and number to save a new entry to the phonebook.");
+  console.log("  1. Password ONLY to display all entries in the phonebook, or");
+  console.log(
+    "  2. Password, name, and number to save a new entry to the phonebook."
+  );
   process.exit(1);
 }
 
-const [newName, newNumber] = args;
+const [password, newName, newNumber] = args;
+
+const url = `mongodb+srv://fullstack:${password}@fullstackopen-a7hji.mongodb.net/note-app?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
 
 if (newName && newNumber) {
   const person = new Person({
