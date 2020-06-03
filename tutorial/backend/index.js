e5d56fd1449fb5c9e489b561afd7ff4446d33b1d
zegheim <justinarista@gmail.com>
2020-06-03 16:51:22 +0700

    front end uses relative uri; add build dir to backend subtree

--- tutorial/backend/index.js
+++ tutorial/backend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 
 const requestLogger = (req, res, next) => {
