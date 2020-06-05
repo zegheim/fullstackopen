8bf98690c6f1cc6710ddba64ac678c3e13a4f588
zegheim <justinarista@gmail.com>
2020-06-05 17:01:40 +0700

    renamed backend to server

---
+++ models/note.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const url = process.env.MONGODB_URI;
+
+console.log("connecting to", url);
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then((res) => console.log("connected to MongoDB"))
+  .catch((e) => console.log("error connecting to MongoDB", e.message));
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (doc, returnedObj) => {
+    returnedObj.id = returnedObj._id.toString();
+    delete returnedObj._id;
+    delete returnedObj.__v;
+  },
+});
+
+module.exports = mongoose.model("Note", noteSchema);
