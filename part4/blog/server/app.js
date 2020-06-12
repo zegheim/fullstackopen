9d3fe12b53caf0fbfc2493c2677e909fdacc4288
zegheim <justinarista@gmail.com>
2020-06-13 01:46:10 +0700

    finished exercise 4.18

--- part4/blog/server/app.js
+++ part4/blog/server/app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const blogsRouter = require("./controllers/blogs");
+const loginRouter = require("./controllers/login");
 const usersRouter = require("./controllers/users");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
@@ -27,6 +28,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/login", loginRouter);
 app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
